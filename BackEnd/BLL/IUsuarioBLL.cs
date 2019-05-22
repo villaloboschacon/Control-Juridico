36a00b20c88c139ad0a214de6c285ad0b6ed7e14
JORGE VILLALOBOS CHACON <villaloboschacon>
2019-05-21 23:48:14 -0600

    Recuperación proyecto.

--- BackEnd/BLL/IUsuarioBLL.cs
+++ BackEnd/BLL/IUsuarioBLL.cs
@@ -1,9 +1,5 @@
 ﻿using BackEnd.Model;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BackEnd.BLL
 {
--- BackEnd/DAL/DALGenericoImpl.cs
+++ BackEnd/DAL/DALGenericoImpl.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
 using BackEnd.Model;
-using System.Data.Entity;
 
 namespace BackEnd.DAL
 {
